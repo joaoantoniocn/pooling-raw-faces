bc93050c3796c29152f7a00b84f5d6a2fc412dc7
belleaf <belleaferreira@gmail.com>
2016-05-03 16:49:02 -0300

    Código para separar e preparar a base LFW
    - Separa a base em treino e teste

---
+++ separarbase.m
@@ -0,0 +1,22 @@
+function [ ] = separarbase( file_name  )
+
+ file_name = [file_name, '/'];
+    pastas = dir(file_name);
+    
+    for i = 3 : length(pastas)
+        
+       nome_pasta = pastas(i).name;
+       nome_pasta_completo = [file_name, nome_pasta, '/'];
+       nome_fotos = dir([nome_pasta_completo, '/*jpg']);
+       
+       mkdir(['./treino/', nome_pasta]);
+       for j = 1 : length(nome_fotos)
+           
+            if j <= (length(nome_fotos)/2)
+             
+              movefile([ nome_pasta_completo, '/', nome_fotos(j).name ], ['./treino/', nome_pasta, '/', nome_fotos(j).name ])
+            end            
+       end               
+    end    
+end
+
