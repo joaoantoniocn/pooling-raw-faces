00113dab86427f5a840b893b93e86a58fd877492
Joao Antonio C. Nunes <joaoantoniocn@gmail.com>
2016-05-12 16:53:36 -0300

    update

--- treinar.m
+++ treinar.m
@@ -69,9 +69,9 @@ function [lfwInputs, lfwTargets, legenda, legenda2, net] = treinar( )
 
 
     % Setup Division of Data for Training, Validation, Testing
-    net.divideParam.trainRatio = 100/100;
-    net.divideParam.valRatio = 0/100;
-    net.divideParam.testRatio = 0/100;
+    net.divideParam.trainRatio = 70/100;
+    net.divideParam.valRatio = 15/100;
+    net.divideParam.testRatio = 15/100;
     
     [net,tr] = train(net,x,t);
     
